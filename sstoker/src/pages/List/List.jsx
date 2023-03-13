34ba2975c9d0ddff1b76af9e5547228a5d2b6e90
Sakshi Rane <76606602+sakshi2431-debug@users.noreply.github.com>
2023-03-14 03:16:14 +0530

    Add files via upload

---
+++ sstoker/src/pages/List/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/header/Header"
+import Navbar from "../../components/navbar/Navbar"
+
+const List =() =>{
+    return(
+        <div>
+            <Header/>
+            <Navbar/>
+        </div>
+    )
+}
